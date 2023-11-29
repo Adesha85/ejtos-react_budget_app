da419a8163b9eb68cbcb6c1732dea218c8aed6c1
Adesha <adeshademmin@yahoo.com>
2023-11-28 20:08:27 -0500

    New Changes with issues to Location.js file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
-//import { FaTimesCircle } from 'react-icons/fa';
+import { FaMinus, FaPlus } from 'react-icons/fa';
+//import { FaPlus } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
     const { dispatch, Location } = useContext(AppContext);
@@ -26,15 +27,40 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td align="center"><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+          <td>{props.name}</td>
+          <td>{Location}{props.cost}</td>
+          
+            <td onClick={event => increaseAllocation(props.name)}>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '24px', height: '24px', borderRadius: '50%', backgroundColor: 'green' }}>
+                <FaPlus color="white" />
+              </div>
+            </td>
+          
+            <td onClick={event => decreaseAllocation(props.name)}>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '24px', height: '24px', borderRadius: '50%', backgroundColor: 'red' }}>
+                <FaMinus color="white" />
+              </div>
+            </td>
+          
+
+          <td align="center"><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
-    );
+      );
 
 };
 
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,29 +1,43 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+//import "./StyleCurrency.css" ;
 
 const Location = () => {
   const {dispatch } = useContext(AppContext);
 
-    const changeLocation = (val)=>{
-            dispatch({
-                type: 'CHG_LOCATION',
-                payload: val,
-            })
-    }
-    
+	const changeLocation = (val)=>{
+			dispatch({
+				type: 'CHG_LOCATION',
+				payload: val,
+			})
+	}
 
   return (
-        <div className='alert alert-secondary'> Currency {
-      <select name="Location" id="Location" onChange={(event)=>changeLocation(event.target.value)}>
-         
-         <option value="$">$ Dollar</option>
-        <option value="£">£ Pound</option>
-        <option value="€">€ Euro</option>
-        <option value="₹">₹ Ruppee</option>
-      </select>	
-      }	
-    </div>
-    );
+    <div className='alert alert-secondary'>
+    <label style={{marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}} >Currency
+  <select name='hover_color'id="currency" onChange={(event)=>changeLocation(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}}>
+    <option style={{color:'black'}} value="£">£ Pound</option>
+    <option style={{color:'black'}} value="$">$ Dollar</option>
+    <option style={{color:'black'}} value="€">€ Euro</option>
+    <option style={{color:'black'}} value="₹">₹ Rupee</option>
+  </select>	
+  </label>
+      
+</div>
+);
 };
 
 export default Location;
+
+
+
+
+
+
+
+
+
+
+
+
+
